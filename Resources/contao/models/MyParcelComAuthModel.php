cc1c9cb40441b7b289db2245fad81232cbb7e89b
coastforge-cro <claudio.ross@kuestenschmiede.de>
2020-04-20 14:50:45 +0200

    Fix filename & add models

---
+++ Resources/contao/models/MyParcelComAuthModel.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace MyParcelCom\ContaoApi\Resources\contao\models;
+
+
+use Contao\Model;
+
+class MyParcelComAuthModel extends Model
+{
+    protected static $strTable = 'tl_myparcelcom_api_auth';
+}
